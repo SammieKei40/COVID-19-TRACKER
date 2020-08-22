41d174bbccc0d7ae6a5a9603a8001caeed3ad0a9
Sammie Kei <58369899+SammieKei40@users.noreply.github.com>
2020-08-22 05:08:44 -0700

    js.js

--- js.js
+++ js.js
@@ -1,25 +1,24 @@
-const NavSlide = function(){
-	const nice = document.querySelector('.nice');
-	const Nav = document.querySelector('.Nav-links');
-	const NavLinks = document.querySelectorAll('.Nav-links li');
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
-	
-	nice.addEventListener('click', function(){
-		//Toggle Nav
-		Nav.classList.toggle('Nav-active');
-	
-		//Animate Links
-	NavLinks.forEach((link, index) =>{
-		if(link.style.animation){
-			link.style.animation = '';
-		}else{
-			link.style.animation = `NavLinkFade 0.5s ease forwards ${index / 7 + 2}s
-		}
-	});
+    
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
 
-	//nice animation
-	nice.classList.toggle('toggle');
-});
+        navLinks.forEach((links, index) => {
+            if(links.style.animation){
+                links.style.animation = "";
+            }else {
+                links.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
+    });
+
+        burger.classList.toggle('toggle');
+    });
 
 }
-NavSlide();
+
+
+navSlide();
